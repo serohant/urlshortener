9ac34f5b046254aaded8f977b6a12b3f27bf70d2
serohan <53527873+serohant@users.noreply.github.com>
2023-03-02 17:22:39 +0300

    Add files via upload

---
+++ URLShortener.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class URLShortener 
+{
+    public $urls;
+
+    public function __construct($urls){
+        $this->urls = $urls;    
+    }
+
+    public function getURLs(){
+        return $this->urls;
+    }
+
+    public function checkName($name){
+        if(isset($this->urls[$name])){
+           return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public function addURL($name,$URL){
+        if(!$this->checkName($name)){
+            $this->urls[$name] = array($name => $URL);
+            file_put_contents('urls.json',json_encode($this->urls,JSON_PRETTY_PRINT));
+        }else{
+            return "shortname exist";
+        }
+    }
+
+    public function removeURL($name){
+        if($this->checkName($name)){
+            unset($this->urls[$name]);
+            file_put_contents('urls.json',json_encode($this->urls,JSON_PRETTY_PRINT));
+        }else{
+            return "name not exist";
+        }
+    }
+}
+
+?>
